76bf0d2b4bfda4ac10e9ed71de432f2b6c38b8de
JoshZastrow <j.a.zastrow.jr@gmail.com>
2017-06-09 12:41:36 -0400

    format Video Stream, start up data write file

--- VideoStream.py
+++ VideoStream.py
@@ -10,26 +10,7 @@ cap = cv2.VideoCapture(0)
 print('Beginning Capture Device opening...\n')
 print('Capture device opened?', cap.isOpened())
 
-##while True:
-##
-##    ret, frame = cap.read()
-##    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-##
-##    cv2.imshow('frame', gray_image)
-##
-##    if cv2.waitKey(1) & 0xFF == ord('q'):
-##        break
-
 running = "n"
-
-##for i in range(0,80):
-##    try:
-##        c = sys.stdin.read(1)
-##        running = c
-##    except IOError:
-##        pass
-##    finally:
-##        pass
 
 
 def newAverageColorGrayscale(image):
@@ -38,157 +19,56 @@ def newAverageColorGrayscale(image):
     input: image object from cv2
     output: singel pixel value, would return 3 values if we were not doing grayscale
     '''
-    average_color_per_row = np.average(image, axis = 0)
-    average_color = np.average(average_color_per_row, axis = 0)
+    average_color_per_row = np.average(image, axis=0)
+    average_color = np.average(average_color_per_row, axis=0)
     return average_color
 
-#initialize average color for something to compare to
+
+# initialize average color for something to compare to
 average_color = 0
 
 while True:
-    #initialize camera
+    # initialize camera
     ret, frame = cap.read()
 
-    #take a picture
+    # take a picture
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #show the picture
+    # show the picture
     cv2.imshow('frame', image)
 
-    #listen for keyboard input
+    # listen for keyboard input
     status = cv2.waitKey(1) & 0xFF
+
     if status == ord('b'):
-        print ("bad status")
+        print("bad status")
         running = "b"
-        
+
     elif status == ord('g'):
-        print ("back to normal")
+        print("back to normal")
         running = "g"
+
     elif status == ord('n'):
         running = "n"
-                     
-        
+
     if running == "g":
-        if (abs(newAverageColorGrayscale(image)-average_color) > 1):
+        if (abs(newAverageColorGrayscale(image) - average_color) > 1):
             print('motion detected')
-            
+
         average_color = newAverageColorGrayscale(image)
-        cv2.imwrite("/home/pi/sentient-cnc/images/good{}.jpg".format(datetime.datetime.now()), image)
+        cv2.imwrite(
+            "/home/pi/sentient-cnc/images/good{}.jpg"
+            .format(datetime.datetime.now()), image)
+
     elif running == "b":
-        cv2.imwrite("/home/pi/sentient-cnc/images/bad{}.jpg".format(datetime.datetime.now()), image)
-
-            
-
-            
-
-# Release the capture
-cap.release()
-cv2.destroyAllWindows()
-
-#Save image with file name = timestamp and classification
-#classification should be toggled by terminal
-import cv2
-import sys
-import numpy as np
-import datetime
-import time
-
-cap = cv2.VideoCapture(0)
-
-
-print('Beginning Capture Device opening...\n')
-print('Capture device opened?', cap.isOpened())
-
-##while True:
-##
-##    ret, frame = cap.read()
-##    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-##
-##    cv2.imshow('frame', gray_image)
-##
-##    if cv2.waitKey(1) & 0xFF == ord('q'):
-##        break
-
-running = "n"
-
-##for i in range(0,80):
-##    try:
-##        c = sys.stdin.read(1)
-##        running = c
-##    except IOError:
-##        pass
-##    finally:
-##        pass
-
-
-def newAverageColorGrayscale(image):
-    '''
-    Finds average color of an image's pixels
-    input: image object from cv2
-    output: singel pixel value, would return 3 values if we were not doing grayscale
-    '''
-    average_color_per_row = np.average(image, axis = 0)
-    average_color = np.average(average_color_per_row, axis = 0)
-    return average_color
-
-#initialize average color for something to compare to
-average_color = 0
-
-while True:
-    #initialize camera
-    ret, frame = cap.read()
-
-    #take a picture
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    #show the picture
-    cv2.imshow('frame', image)
-
-    #listen for keyboard input
-    status = cv2.waitKey(1) & 0xFF
-    if status == ord('b'):
-        print ("bad status")
-        running = "b"
-        
-    elif status == ord('g'):
-        print ("back to normal")
-        running = "g"
-    elif status == ord('n'):
-        running = "n"
-                     
-        
-    if running == "g":
-        if (abs(newAverageColorGrayscale(image)-average_color) > 1):
-            print('motion detected')
-            
-        average_color = newAverageColorGrayscale(image)
-        cv2.imwrite("/home/pi/sentient-cnc/images/good{}.jpg".format(datetime.datetime.now()), image)
-    elif running == "b":
-        cv2.imwrite("/home/pi/sentient-cnc/images/bad{}.jpg".format(datetime.datetime.now()), image)
-
-            
-
-            
-=======
-import numpy as np
-
-cap = cv2.VideoCapture(0)
-
-print('Beginning Capture Device opening...\n')
-print('Capture device opened?', cap.isOpened())
-
-while True:
-
-    ret, frame = cap.read()
-    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame', gray_image)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imwrite(
+            "/home/pi/sentient-cnc/images/bad{}.jpg"
+            .format(datetime.datetime.now()), image)
 
 
 # Release the capture
 cap.release()
 cv2.destroyAllWindows()
 
+# Save image with file name = timestamp and classification
+# classification should
