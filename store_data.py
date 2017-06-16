9f23897fdcd41a391bf2cdf786e7ac1327c27383
Josh Zastrow <j.a.zastrow.jr@gmail.com>
2017-06-16 18:58:48 -0400

    trying to store image object to Cloud... no luck yet

--- store_data.py
+++ store_data.py
@@ -2,12 +2,14 @@ from oauth2client.client import GoogleCredentials
 from google.cloud import datastore
 import datetime
 import numpy as np
+import pickle
 import os
+import cv2
 import sys
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = \
     "/Users/joshua/Documents/Sentient-CNC/SentientCNC-3c8c6f014588.json"
-sys.exit()
+
 
 class data_handler():
     """
@@ -31,8 +33,8 @@ class data_handler():
 
         # Creates an authorized service object to make authenticated
         # requests to Google Cloud API using the API's client libraries.
+        self.sensor_node = sensor_node
         self.client = datastore.Client(project_id)
-        self.device = self.client.key(sensor_node)
         self.access = GoogleCredentials.get_application_default()
 
     def write(self, sensor_data, timestamp=None):
@@ -48,18 +50,16 @@ class data_handler():
 
         # Create key for timestamp
         if not timestamp:
-            timestamp = datetime.datetime.now()
+            timestamp = str(datetime.datetime.now())
 
         data_snapshot = self.client.key(self.sensor_node, timestamp)
-
-        print('data key generated:', data_snapshot)
 
         entry = datastore.Entity(data_snapshot)
         entry.update(sensor_data)
         entry.update({'created': timestamp})
 
         # Push the data entry to the cloud
-        self.client.put(entry, credentials=self.access)
+        self.client.put(entry)
         print('data pushed to cloud. Entry', entry)
 
         # Check to make sure entry was added to DataStore
@@ -82,15 +82,41 @@ class data_handler():
 
 if __name__ == "__main__":
 
-    curr_time = datetime.datetime.now()
-    data_package = {'camera': np.arange(30000).reshape((100, 100, 3)),
-                    'mic': np.arange(100).reshape((1, 100)),
+    # Get current time for data key storage and 'created' parameter
+    curr_time = str(datetime.datetime.now())
+
+    # image snapshot
+    capture = cv2.VideoCapture(0)
+    ret, frame = capture.read()
+    capture.release()
+    cv2.destroyAllWindows()
+
+    # Trying to convert the cv2 frame object into something useable... 
+    frame_list = np.array(frame)
+    frame_list = frame_list.tolist()
+    frame_pickle = pickle.dumps(frame)
+
+    # info
+    print('frame captured. converted to list. shape:', len(frame_list))
+    print()
+    print('frame size:', sys.getsizeof(frame))
+    print('frame list size:', sys.getsizeof(frame_list))
+    print('frame pickle size:', sys.getsizeof(frame_pickle))
+    print()
+    print('frame pickle:', frame_pickle[0:3])
+
+    # Psuedo sensor data dictionary
+    # Collected values of all sensors at a given time instance
+    data_package = {'camera': frame_list,
+                    'mic': [2, 3, 3, 3],
                     'created': curr_time,
                     'label': 'Running'}
 
     print('Executing test run....\n\n\tCurrent time: {}'
-          .format(curr_time))
+          .format(curr_time), '\ndata type:', type(curr_time))
 
-    data = data_handler()
-
-    data.write(data_package, curr_time)
+    # Can't write image object to NoSQL database... 
+    ##################################################
+    # data = data_handler()
+    # data.write(sensor_data=data_package, timestamp=curr_time)
+    ##########################################################
