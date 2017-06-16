12e68786488daf1a1d60681ed9800f9951cdd618
Josh Zastrow <j.a.zastrow.jr@gmail.com>
2017-06-16 12:47:32 -0400

    Create handler object, add write functionality

--- store_data.py
+++ store_data.py
@@ -2,28 +2,56 @@ from google.cloud import datastore
 import datetime
 
 
-class data_writer():
+class data_handler():
+    """
+    Object designed to handle storage of incoming sensor data.
+    Args:
+    project_id: string
+        Project ID per Google Cloud's project ID (see Cloud Console)
+    sensor_node: string
+        Identifier for sensor gateway. This is used as a primary key for
+        storing data entries (so data can be viewed per device)
+    """
+    def __init__(self,
+                 project_id='sentientcnc-1',
+                 sensor_node='CNCmill_001'):
 
-    def __init__(self, project_id='sentientcnc-1'):
-        self.project = datastore.Client(project_id)
-        pass
+        # Creates an authorized service object to make authenticated
+        # requests to Google Cloud API using the API's client libraries.
+        self.client = datastore.Client(project_id)
+        self.device = self.client.key(sensor_node)
 
+    def write(self, sensor_data, timestamp=None):
+        """
+        writes the sensor data to the cloud storage
 
-def create_client(project_id):
-    '''
-    Creates an authorized service object to make authenticated
-    requests to Google Cloud API using the API's client libraries
-    input: Google Cloud project ID
-    output: client object
-    '''
+        Args:
+        sensor_data: dict
+            dictionary of sensor nodes as keys and data as parameters
+        timestamp: string
+            timestamp of when the data was recorded
+        """
 
-    return datastore.Client(project_id)
+        # Create key for timestamp
+        if not timestamp:
+            data_snapshot = self.client.key(self.sensor_node,
+                                            datetime.datetime)
+        else:
+            data_snapshot = self.client.key(self.sensor_node,
+                                            timestamp)
+
+        entry = datastore.Entity(data_snapshot)
+        entry.update(sensor_data)
+
+        client.put(entry)
+
+        with client.transaction():
 
 
 def add_task(client, description):
     key = client.key('Task')
 
-    task = datastore.Entity(key, exclude_from_indexes=['description'])
+    task = datastore.entity(key, exclude_from_indexes=['description'])
 
     task.update({
         'created': datetime.datetime.utcnow(),
@@ -31,7 +59,8 @@ def add_task(client, description):
         'done': False
     })
 
-    client.put(task)
+    client.put(task
+
 
     return task.key
 
@@ -63,5 +92,3 @@ def list_tasks(client):
     query.order = ['created']
 
     return list(query.fetch())
-
-
