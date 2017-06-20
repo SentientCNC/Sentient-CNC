0a38a3268675637992184e8606587ab3ec55587a
Josh Zastrow <j.a.zastrow.jr@gmail.com>
2017-06-20 12:41:47 -0400

    Add meta data, if name = main and comment out pyaudio so I can run the code

--- VideoStream.py
+++ VideoStream.py
@@ -4,40 +4,7 @@ import numpy as np
 import datetime
 # import time
 import pyaudio
-from audiostream import *
-
-cap = cv2.VideoCapture(0)
-
-moments = []
-
-print('Beginning Capture Device opening...\n')
-print('Capture device opened?', cap.isOpened())
-
-running = "n"
-
-# chunk = 1024
-
-# p = pyaudio.PyAudio()
-# fulldata = np.array([])
-# dry_data = np.array([])
-
-# stream = p.open(format=pyaudio.paInt16,
-#                channels=1,
-#                rate=16000,
-#                input=True,
-#                frames_per_buffer=1024)
-
-# in_speech_bf = False
-
-# ##decoder.start_utt()
-
-# while True:
-#    if stream.is_stopped():
-#        stream.start_stream()
-#    buf = stream.read(1024)
-#    if buf:
-#        stream.stop_stream()
-#        decoder.process_raw(buf, False, False)
+# from audiostream import *
 
 
 def newAverageColorGrayscale(image):
@@ -67,61 +34,110 @@ def test():
     return True
 
 
-# initialize average color for something to compare to
-average_color = 0
+if __name__ == '__main__':
 
-# from audiostream
-tt = TapTester()
+    cap = cv2.VideoCapture(0)
 
-counter = 0
-run = True
-while run:
-    # listen
-    audio = tt.listen()
+    moments = []
+    metadata = {
+        'frame_width': cap.get(id=3),
+        'frame_height': cap.get(id=4),
+        'fps': cap.get(id=5),
+        'brightness': cap.get(id=10),
+        'contrast': cap.get(id=11),
+        'saturation': cap.get(id=12),
+        'exposure': cap.get(id=15)
+    }
 
-    # initialize camera
-    ret, image = cap.read()
+    print('Beginning Capture Device opening...\n')
+    print('Capture device opened?', cap.isOpened())
+    print('Video Capture Parameters\n-----------------\n\n')
 
-    # process the picture
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    for k in metadata.keys():
+        print('{}: {}'.format(k, metadata[k]))
 
-    # how the picture
-    cv2.imshow('image', image)
+    running = "n"
 
-    # listen for keyboard input
-    status = cv2.waitKey(1) & 0xFF
-    if status == ord('b'):
-        print ("bad status")
-        running = "b"
+    # chunk = 1024
 
-    elif status == ord('g'):
-        print ("back to normal")
-        running = "g"
-    elif status == ord('n'):
-        running = "n"
+    # p = pyaudio.PyAudio()
+    # fulldata = np.array([])
+    # dry_data = np.array([])
 
-    if running == "g":
-        if (abs(newAverageColorGrayscale(image) - average_color) > 1):
-            print('motion detected')
+    # stream = p.open(format=pyaudio.paInt16,
+    #                channels=1,
+    #                rate=16000,
+    #                input=True,
+    #                frames_per_buffer=1024)
 
-        average_color = newAverageColorGrayscale(image)
-        cv2.imwrite("/home/pi/sentient-cnc/images/good{}.jpg"
-                    .format(datetime.datetime.now()), image)
-    elif running == "b":
-        cv2.imwrite("/home/pi/sentient-cnc/images/bad{}.jpg"
-                    .format(datetime.datetime.now()), image)
+    # in_speech_bf = False
 
-    moments.append({"audio": audio,
-                    "image": image,
-                    "timestamp": datetime.datetime.now()
-                    })
-    if counter > 30:
-        run = False
-    counter += 1
+    # ##decoder.start_utt()
 
-for moment in moments:
-    print(moment["audio"], moment["timestamp"])
+    # while True:
+    #    if stream.is_stopped():
+    #        stream.start_stream()
+    #    buf = stream.read(1024)
+    #    if buf:
+    #        stream.stop_stream()
+    #        decoder.process_raw(buf, False, False)
 
-# Release the capture
-cap.release()
-cv2.destroyAllWindows()
+
+    # initialize average color for something to compare to
+    average_color = 0
+
+    # from audiostream
+    tt = TapTester()
+
+    counter = 0
+    run = True
+    while run:
+        # listen
+        audio = tt.listen()
+
+        # initialize camera
+        ret, image = cap.read()
+
+        # process the picture
+        # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # how the picture
+        cv2.imshow('image', image)
+
+        # listen for keyboard input
+        status = cv2.waitKey(1) & 0xFF
+        if status == ord('b'):
+            print ("bad status")
+            running = "b"
+
+        elif status == ord('g'):
+            print ("back to normal")
+            running = "g"
+        elif status == ord('n'):
+            running = "n"
+
+        if running == "g":
+            if (abs(newAverageColorGrayscale(image) - average_color) > 1):
+                print('motion detected')
+
+            average_color = newAverageColorGrayscale(image)
+            cv2.imwrite("/home/pi/sentient-cnc/images/good{}.jpg"
+                        .format(datetime.datetime.now()), image)
+        elif running == "b":
+            cv2.imwrite("/home/pi/sentient-cnc/images/bad{}.jpg"
+                        .format(datetime.datetime.now()), image)
+
+        moments.append({"audio": audio,
+                        "image": image,
+                        "timestamp": datetime.datetime.now()
+                        })
+        if counter > 30:
+            run = False
+        counter += 1
+
+    for moment in moments:
+        print(moment["audio"], moment["timestamp"])
+
+    # Release the capture
+    cap.release()
+    cv2.destroyAllWindows()
